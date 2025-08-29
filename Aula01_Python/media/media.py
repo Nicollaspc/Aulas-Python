4ca663e5539b22f8ae2c940436f4377ab37255e0
Nicollas Nascimento <nicollasnascimento.pc@gmail.com>
2025-08-28 22:34:39 -0300

    Primeira aula de python na faculdade

---
+++ Aula01_Python/media/media.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite o primeiro numero: \n"))
+num2 = float(input("Digite o segundo numero: \n"))
+num3 = float(input("Digite o terceiro numero: \n"))
+media = (num1 + num2 + num3) /3
+print(f"A média dos numeros: {num1}, {num2}, {num3} = {media:.2f}")
